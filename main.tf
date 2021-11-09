3270e129677826f40d18a494b69454ae625bc9df
Sivasankar Nara <nara.sivasankar@gmail.com>
2021-11-09 02:10:27 -0500

    new vms

--- main.tf
+++ main.tf
@@ -19,3 +19,33 @@ resource "google_compute_instance" "myvm" {
         }
         }
 }
+
+resource "google_compute_instance" "myvm2" {
+        name = "myfirstvm2"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+                access_config{}
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+        }
+        }
+}
+
+resource "google_compute_instance" "myvm3" {
+        name = "myfirstvm3"
+        machine_type = "f1-micro"
+        zone = "us-central1-a"
+        network_interface {
+                network = "default"
+                access_config{}
+        }
+        boot_disk {
+                initialize_params {
+                        image = "debian-cloud/debian-10"
+        }
+        }
+}
